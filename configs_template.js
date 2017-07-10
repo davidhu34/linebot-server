70395da903268cefd62ae15267227fa9c3bc5edb
davidhu34 <b01901133@gmail.com>
2017-07-10 13:21:45 +0800

    line iot version

--- configs_template.js
+++ configs_template.js
@@ -4,5 +4,13 @@ module.exports = {
 		CHANNEL_ID: 0,
 		CHANNEL_SECRET: '',
 	    CHANNEL_ACCESS_TOKEN: ''
+	},
+	mqtt: {		
+		organizationId: '',
+		type: '',
+		deviceType: '',
+		deviceId: '',
+		username: '',
+		password: ''
 	}
 }
